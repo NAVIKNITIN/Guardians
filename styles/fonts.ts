f1d24db08f1692c5717898e55decdc234dc1b9fc
NAVIKNITIN <naviknitin143@gmail.com>
2026-04-17 00:06:55 +0530

    font changes

--- styles/fonts.ts
+++ styles/fonts.ts
@@ -12,7 +12,7 @@ export const nexaFont = localFont({
 
 export const qasbyneFont = localFont({
   src: [
-    { path: "../public/fonts/qasbyne/Qasbyne.otf", weight: "100" },
+    { path: "../public/fonts/qasbyne/Qasbyne.otf", weight: "100 350" },
     { path: "../public/fonts/qasbyne/Qasbyne-Regular.woff2", weight: "300" },
     { path: "../public/fonts/qasbyne/Qasbyne-Medium.woff2", weight: "500" },
     { path: "../public/fonts/qasbyne/Qasbyne-Bold.woff2", weight: "700" },
